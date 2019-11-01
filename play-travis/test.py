1705df83190bacd66dea769ad5dd34d060b21e40
Andrey Romanov <aromanov.app@gmail.com>
2019-11-01 12:20:38 +0300

    added one more test

--- play-travis/test.py
+++ play-travis/test.py
@@ -9,6 +9,9 @@ class NumbersTest(unittest.TestCase):
     def test_equal2(self):
         self.assertEqual(2 + 2, 4)
 
+    def test_equal3(self):
+        self.assertEqual(3 + 3, 6)
+
 
 if __name__ == '__main__':
     unittest.main()
